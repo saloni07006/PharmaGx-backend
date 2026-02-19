54adc096f60626682dbd1f64e6e8675ee08a7b55
Saloni Tanmor <saloni.tanmor1@gmail.com>
2026-02-20 03:41:11 +0530

    config new update

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
-MODEL_PATH = str(PROJECT_ROOT / "models" / "pharmaguard_random_forest.pkl")
-FEATURES_PATH = str(PROJECT_ROOT / "models" / "model_features.pkl")
+MODEL_PATH = str("models" / "pharmaguard_random_forest.pkl")
+FEATURES_PATH = str("models" / "model_features.pkl")
